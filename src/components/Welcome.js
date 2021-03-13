efcd758f42e08e10230664cf85db0db27bac65d8
AnDx25 <a.dixit@iiitmanipur.ac.in>
2021-03-14 00:08:37 +0530

    Welcome Component v2

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,8 @@
 // write code for Welcome component here
-const Welcome=({name})=>{
+const Welcome=(props)=>{
     return(
         <div>
-            <h1>Hey ! {name}</h1>
+            <h1>Hey !{props.name}</h1>
             <h2>Welcome to Newton School.</h2>
         </div>
     )
